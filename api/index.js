b6facb2002ba0f0bf2d50b75b3ff7714ba09f242
fikasp <fikasp@gmail.com>
2023-10-04 21:53:47 +0200

    Rename server to api

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
-import app from "../server/server.js"
+import express from 'express'
+import { PORT } from './config/config.js'
+import api from './controllers/api.controller.js'
+
+const app = express()
+
+app.use('/api', api)
+
+app.listen(PORT, () => {
+	console.log(`
+Server started...
+http://localhost:${PORT}`)
+})
 
 export default app
