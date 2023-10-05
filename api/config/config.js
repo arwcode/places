8adc5298d1269d858b9015086a9a4a52028afb64
fikasp <fikasp@gmail.com>
2023-10-05 20:20:54 +0200

    Rename database to places

--- api/config/config.js
+++ api/config/config.js
@@ -2,4 +2,4 @@ export const PORT = process.env.PORT || 5000
 
 export const MONGODB =
 	process.env.MONGODB ||
-	'mongodb+srv://arw:arw@arw.j4kasco.mongodb.net/mern?retryWrites=true&w=majority'
+	'mongodb+srv://arw:arw@arw.j4kasco.mongodb.net/places?retryWrites=true&w=majority'
